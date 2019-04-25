8a3087f59f4887f321db49e921261befe3ee24d6
lightnin-joe <49998359+lightnin-joe@users.noreply.github.com>
2019-04-25 17:02:36 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ then
     git commit -a -m "Version auto-incremented  - $TRAVIS_JOB_NUMBER [ci skip]"
     gulp build
     gulp release
-    git remote add github https://$GITHUB_TOKEN@github.com/bywatersolutions/koha-plugin-kitchen-sink
+    git remote add github https://$GITHUB_TOKEN@github.com/lightnin-joe/koha-test
     git fetch --all
     git push github HEAD:master
   fi
